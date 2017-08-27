28382091145f8ed4bba56d26910ed57b500121cc
acordobag <amcordobag@gmail.com>
2017-08-27 17:32:58 -0600

    lol fix

--- models/account_invoice_inherited.py
+++ models/account_invoice_inherited.py
@@ -3,5 +3,5 @@ from openerp import models, fields, api
 
 class Account(models.Model):
     _inherit = 'account.invoice'
-    date_day = date_effective = fields.Date('Fecha de cita')
-    ccss_ref = name  = fields.Char('Número de referencia',20)
+    date_day = fields.Date('Fecha de cita')
+    ccss_ref = fields.Char('Número de referencia',20)
