f593766ddea4a0522e4836d078c9a7e7018e6735
acordobag <amcordobag@gmail.com>
2017-09-07 23:33:56 -0600

    test num2words

--- models.py
+++ models.py
@@ -1,9 +1,17 @@
 #-*- coding: utf-8 -*-
 
 from openerp import models, fields, api
+from num2words import num2words
 
 class invoice_extend(models.Model):
     _inherit = 'account.invoice'
 
     date_day = fields.Datetime('Fecha cita')
     ccss_ref = fields.Char('Número receta')
+    total_amount_text=fields.Char('Total en letras:')
+
+    @api.depends('amount_total')
+    @api.onchange('amount_total')
+    def amount_total_to_text(self):
+        self.total_amount_text= num2words(self.amount_total, lang='fr')
+
